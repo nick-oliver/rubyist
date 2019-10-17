85f2bacd98a89154ba79f4ed1b5c1344adadab95
Nick Oliver <nick.oliver@rubydev.safehold.local>
2019-10-17 12:22:03 -0500

    re-organize examples and exercises

--- chapt_2/ticket.rb
+++ chapt_2/ticket.rb
@@ -18,11 +18,13 @@ end
 def ticket.price
   5.50
 end
+# Added by the exercise in chapter two
 def ticket.print_details(*x)
   x.each { |detail| puts "This ticket is #{detail}" }
 end
 
-
+## Original example format with Print and Puts arranged so that everything is
+## displayed correctly, when the program is run.
 
 # print "This ticket is for: "
 # print ticket.event + ", at "
@@ -35,9 +37,10 @@ end
 # print "and it costs $"
 # puts "%.2f." % ticket.price
 
-
+# refactoring of the print and puts statements above
 puts "This ticket is for: #{ ticket.event }, at #{ ticket.venue }.\n" +
 "The performer is #{ ticket.performer }.\n" +
 "The seat is #{ ticket.seat }, " +
 "and it costs $#{ "%.2f." % ticket.price }"
-ticket.print_details("Non-Transferable","Non-Refundable","Non-smoking")
+#This provides the variables to comlete the array iteration in the last "def".
+ticket.print_details("non-refundable","non-transferable","in a no-smoking area.")
