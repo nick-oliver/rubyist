a79241b9545fd22e7ed12e7a3449a0062a869e41
Nick Oliver <nick.oliver@rubydev.safehold.local>
2019-10-19 15:39:12 -0500

    Exercise for chapt 3

--- chapt_3/ticket.rb
+++ chapt_3/ticket.rb
@@ -3,13 +3,23 @@ class Ticket
     @venue = venue
     @date = date
   end
-  def set_price(amount)
+  def price=(amount)
     @price = amount
   end
   def price
     @price
   end
-
+  def discount=(percent)
+    @discount = percent
+  end
+  def discount
+    @discount
+  end
+  # testing still - this is probably wrong
+  def total_cost (calculation)
+    @total_cost = ticket.price.to_i * (100 - ticket.discount.to_i) / 100"
+  end
+  # End of experimenting
   def venue
     @venue
   end
@@ -20,8 +30,16 @@ class Ticket
     @price
   end
 end
-th = Ticket.new("Town Hall", "2013-11-12", 63.00)
-cc = Ticket.new("Convention Center", "2014-12-13", 91.00)
-puts "We've created two tickets."
-puts "The first is for a #{th.venue} event on #{th.date}."
-puts "The second is for an event on #{cc.date} at #{cc.venue}."
+ticket = Ticket.new("Town Hall", "2013-11-12")
+ticket.price = 100.00
+puts "The ticket costs $#{"%.2f" % ticket.price}."
+ticket.discount = 15.00
+puts "The discount on this ticket is #{"%.2f" % ticket.discount}%."
+puts "The ticket for #{ticket.venue} has been discounted #{ticket.discount}%."
+# last line - experimenting
+puts "The final cost for the ticket is #{ticket.price.to_i} * (100 - #{ticket.discount.to_i}) / 100)"
+# last line - experimenting
+
+
+# ticket.price = 72.50
+# puts "Whoops -- it just went up. It now costs $#{"%.2f" % ticket.price}."
